6391a45429088b440a251883f0b52d643e8915cc
Julio Cezar <jcezar.t@gmail.com>
2026-03-20 06:06:10 -0300

    initial commit - bytecoin iOS app

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,21 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "791600bd-dcda-457f-add2-74e41256270d"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency: String) {
+        
+    }
+}
